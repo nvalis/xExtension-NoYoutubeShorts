5697701d4fda3380ca3ea0e5872c291cee605204
Novalis <nvalis@users.noreply.github.com>
2025-06-22 16:12:28 +0200

    Add null checks to prevent parse_str() fatal error on URLs without query strings

--- extension.php
+++ extension.php
@@ -3,21 +3,32 @@
 declare(strict_types=1);
 
 class NoYoutubeShortsExtension extends Minz_Extension {
-	public function init(): void {
-		$this->registerHook('entry_before_insert', [$this, 'filterYoutubeShorts']);
-	}
+        public function init(): void {
+                $this->registerHook('entry_before_insert', [$this, 'filterYoutubeShorts']);
+        }
 
-	public function filterYoutubeShorts(FreshRSS_Entry $entry): FreshRSS_Entry {
-		if (parse_url($entry->link(), PHP_URL_HOST) != 'www.youtube.com') {
-			return $entry; // we are only interested in youtube URLs
-		}
-		parse_str(parse_url($entry->link(), PHP_URL_QUERY), $vars);
-		$shortsURL = 'https://www.youtube.com/shorts/' . $vars['v'];
-		$headers = get_headers($shortsURL, true);
+        public function filterYoutubeShorts(FreshRSS_Entry $entry): FreshRSS_Entry {
+                if (parse_url($entry->link(), PHP_URL_HOST) != 'www.youtube.com') {
+                        return $entry; // we are only interested in youtube URLs
+                }
 
-		if ($headers[0] == 'HTTP/1.1 200 OK') {
-			$entry->_isRead(true); // shorts video detected, set entry to read
-		}
-		return $entry;
-	}
+                $query = parse_url($entry->link(), PHP_URL_QUERY);
+                if ($query === null) {
+                        return $entry; // no query string, can't be a video URL
+                }
+
+                parse_str($query, $vars);
+
+                if (!isset($vars['v'])) {
+                        return $entry; // no video ID found
+                }
+
+                $shortsURL = 'https://www.youtube.com/shorts/' . $vars['v'];
+                $headers = get_headers($shortsURL, true);
+
+                if ($headers !== false && isset($headers[0]) && $headers[0] == 'HTTP/1.1 200 OK') {
+                        $entry->_isRead(true); // shorts video detected, set entry to read
+                }
+                return $entry;
+        }
 }
